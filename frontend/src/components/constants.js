339ff019fadb561fbbc6a032150122c3191863a3
Dharmender <dharmender7077@gmail.com>
2025-06-08 11:11:21 +0530

    Next JS project setup

--- frontend/src/components/constants.js
+++
@@ -1,9 +0,0 @@
-export const menuLinks = [
-  { name: "Home", url: "/" },
-  { name: "About", url: "/about" },
-  { name: "Services", url: "/services" },
-  { name: "Portfolio", url: "/portfolio" },
-  { name: "Blog", url: "/blog" },
-  { name: "FAQ", url: "/faq" },
-  { name: "Get Hired", url: "/get-hired" },
-];
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+   <div>
+    <h1>Digital Initiate - A Digital Marketing Agency</h1>
+   </div>
+  );
+}
